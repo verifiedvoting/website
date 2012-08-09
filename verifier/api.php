9c4ae5f5dc9a3b04eea0cac087106138bb1ae55b
tedsuo <ted@radicaldesigns.org>
2012-08-09 01:34:13 +0000

    removed pagination from api, added multiselect for machines

--- verifier/api.php
+++ verifier/api.php
@@ -13,9 +13,6 @@
 //  [[NOTE: If data is GEOJSON, it will be a STRING in the object, not a subobject
 //    It is necesarry to JSON.parse on the clientside]]
 //
-// x-current-page & x-total-results
-//   we put meta information about result paging in the http response headers
-// 200 / 501 status codes depending on resuts
 
 
 
@@ -52,42 +49,45 @@ if($mode=='state'){
     $state = $_GET['state'];
     $county = $_GET['county'];
     
-    $query = "SELECT * FROM machine WHERE";
+    $base_query = "SELECT * FROM machine WHERE";
     if($county){
-      $query .= " county_fips LIKE $county";
+      $base_query .= " county_fips LIKE $county";
       if($state){
-        $query .= " AND";
+        $base_query .= " AND";
       }
     }
     if($state){
-      $query .= " state_fips LIKE $state";
+      $base_query .= " state_fips LIKE $state";
     }
- 
        
-    $query = mysql_escape_string($query);
-       
-    $query .= " AND jurisdiction_type LIKE 'County'";
-    $resource = mysql_query($query);
-  
-    if(mysql_error()){
-        header(' ',true,501);
-      return_json(1,mysql_error());
-    } else if(mysql_num_rows($resource)>0){
-        $data = array();
-      while($get = mysql_fetch_assoc($resource)){
+    $base_query = mysql_escape_string($base_query);
+    $base_query .= " AND jurisdiction_type LIKE 'County'";
+    
+    $data = array();
+    
+    $resource = mysql_query($base_query.' AND (pp_std=1 OR pp_acc=1) LIMIT 4');
+    while($get = mysql_fetch_assoc($resource)){
       $data[] = $get;
-      }
-      if(count($data)>4) {
-        Header('x-total-results: '.count($data));
-        $offset = $_GET['offset'] ? $_GET['offset'] : 0;
-        Header('x-current-offset: '.$offset);
-        $data = array_slice($data,$offset,5);
-      }
-      return_json(0,'',$data);
-    } else {
-    return_json(1,'EMPTY SET - No rows in result');
+    }
+    $resource = mysql_query($base_query.' AND (ev_std=1 OR ev_acc=1) LIMIT 4');
+    while($get = mysql_fetch_assoc($resource)){
+      $data[] = $get;
+    }
+    $resource = mysql_query($base_query.' AND (abs_ballot) LIMIT 4');
+    while($get = mysql_fetch_assoc($resource)){
+      $data[] = $get;
+    }
+    $resource = mysql_query($base_query.' AND (prov_ballot) LIMIT 4');
+    while($get = mysql_fetch_assoc($resource)){
+      $data[] = $get;
     }
     
+    if(count($data)>0){
+      return_json(0,'',$data);  
+    }else {
+      return_json(1,'EMPTY SET - No rows in result ');
+    }
+
   } else {
       header(' ',true,501);
     return_json(0,'missing state and county for machine search');
@@ -119,12 +119,6 @@ if($mode=='state'){
         $data = array();
       while($get = mysql_fetch_assoc($resource)){
       $data[] = $get;
-      }
-      if(count($data)>4) {
-        Header('x-total-results: '.count($data));
-        $offset = $_GET['offset'] ? $_GET['offset'] : 0;
-        Header('x-current-offset: '.$offset);
-        $data = array_slice($data,$offset,5);
       }
       return_json(0,'',$data);
     } else {
